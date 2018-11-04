22e81aad2e4fd6a2109a3eabfe635eb4710fc284
byrne-saw <byrnes.aw@gmail.com>
2018-11-04 10:38:09 -0600

    adding nav bar & footer

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,49 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/logout">Logout</router-link>
+    
+    <div class="header">
+      <b-navbar toggleable="md" type="dark" variant="success">
+
+        <b-navbar-toggle target="nav_collapse"></b-navbar-toggle>
+
+        <div>
+          <!-- Image and text -->
+          <b-navbar variant="faded" type="light">
+            <b-navbar-brand href="#">
+              <img src="https://placekitten.com/g/30/30" class="d-inline-block align-top" alt="BV">
+              A Day
+            </b-navbar-brand>
+          </b-navbar>
+        </div>
+
+        <b-collapse is-nav id="nav_collapse">
+          <!-- Right aligned nav items -->
+          <b-navbar-nav class="ml-auto">
+            <b-nav-item-dropdown right>
+              <!-- Using button-content slot -->
+              <template slot="button-content">
+                <em>User</em>
+              </template>
+              <b-dropdown-item href="#/Logout">Logout</b-dropdown-item>
+            </b-nav-item-dropdown>
+            <b-nav-item href="#/Logout">Logout</b-nav-item>
+          </b-navbar-nav>
+
+        </b-collapse>
+      </b-navbar>
+      <!-- navbar-1.vue -->   
+      <div style="height: 20px"></div> 
     </div>
+
     <router-view/>
+
+    <footer class="footer">
+      <div class="container">
+        <span class="text-muted"> Copyright © 2018. Apple A Day by <a href="http://kingstudio.ro" target="_blank">AByrnesJaunt</a>. All Rights Reserved.
+        </span>
+      </div>
+    </footer>
+  <!-- end app div -->
   </div>
 </template>
 
@@ -15,16 +55,13 @@
   text-align: center;
   color: #2c3e50;
 }
-#nav {
-  padding: 30px;
-}
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+.footer {
+    position: absolute;
+    bottom: 0;
+    width: 100%;
+    height: 60px;
+    line-height: 60px;
+    background-color: #f5f5f5;
 }
 </style>
