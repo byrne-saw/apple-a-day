1301b35e285a0b1bac767f48bb1264895f63fcb6
byrne-saw <byrnes.aw@gmail.com>
2018-11-10 12:31:56 -0600

    cleaning up login page

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,20 +1,26 @@
 <template>
   <div class="login">
     <div class="container">
-      <form v-on:submit.prevent="submit()">
-        <ul>
-          <li class="text-danger" v-for="error in errors">{{ error }}</li>
-        </ul>
-        <div class="form-group">
-          <label>Email:</label>
-          <input type="email" class="form-control" v-model="email">
+      <div class="row">
+        <div class="col"></div>
+        <div class="col">
+          <form v-on:submit.prevent="submit()">
+            <div class="form-group">
+              <label>Email:</label>
+              <input type="email" class="form-control" v-model="email">
+            </div>
+            <div class="form-group">
+              <label>Password:</label>
+              <input type="password" class="form-control" v-model="password">
+            </div>
+            <ul>
+              <li class="text-danger" v-for="error in errors">{{ error }}</li>
+            </ul>
+            <input type="submit" class="btn btn-success" value="Login">
+          </form>
         </div>
-        <div class="form-group">
-          <label>Password:</label>
-          <input type="password" class="form-control" v-model="password">
-        </div>
-        <input type="submit" class="btn btn-success" value="Login">
-      </form>
+        <div class="col"></div>
+      </div>
     </div>
   </div>
 </template>
